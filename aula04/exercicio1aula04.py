6160a28a8f85b721e5cf3f106a37c5a51b9fc64f
matheussf100 <matheussf300@gmail.com>
2026-03-13 13:32:05 -0300

    alteração na variável dimensão

--- aula04/exercicio1aula04.py
+++ aula04/exercicio1aula04.py
@@ -11,17 +11,19 @@
 # 3m² existe um bocal para uma lâmpada.
 
 # Variáveis: potência (watts), largura e comprimento (metros)
+try:
+    potencia= int(input("Informe a potencia:")) # usuário declarando as variáveis necessárias para a execução
+    largura= int(input("Informe a largura:")) # usuário declarando as variáveis necessárias para a execução
+    comprimento= int(input("Informe o comprimento:"))  #usuário declarando as variáveis necessárias para a execução
+    dimensao= 3 * 3 #parte do verificador.
 
-potencia= int(input("Informe a potencia:")) # usuário declarando as variáveis necessárias para a execução
-largura= int(input("Informe a largura:")) # usuário declarando as variáveis necessárias para a execução
-comprimento= int(input("Informe o comprimento:"))  #usuário declarando as variáveis necessárias para a execução
-dimensao= largura * comprimento #parte do verificador.
-
-if potencia>=3 and dimensao==9: # verificação de condição => verdadeira
-    print("1 lampada ilumina o comodo") # Retorno da condição verdadeira
-elif potencia>=3 and dimensao<9: #verificação de condição => falsa.
-    print("precisa de mais uma lampada/bocal por comodo") # Retorno da condição falsa
-elif potencia<3 and dimensao==9: #verificação de condição => falsa
-    print("potencia insuficiente") # Retorno da condição falsa.
-else: #Nenhuma das condições atendidas.
-    print("Potencia e dimensões insuficientes!") #retorno da exceção
+    if potencia>=3 and dimensao==9: # verificação de condição => verdadeira
+        print("1 lampada ilumina o comodo") # Retorno da condição verdadeira
+    elif potencia>=3 and dimensao<9: #verificação de condição => falsa.
+        print("precisa de mais uma lampada/bocal por comodo") # Retorno da condição falsa
+    elif potencia<3 and dimensao==9: #verificação de condição => falsa
+        print("potencia insuficiente") # Retorno da condição falsa.
+    else: #Nenhuma das condições atendidas.
+        print("Potencia e dimensões insuficientes!") #retorno da exceção
+except:
+    print("Erro desconhecido!")
