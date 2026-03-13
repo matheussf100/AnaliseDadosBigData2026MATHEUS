36702e1716d328c8af8f3e27950eb051928b0727
matheussf100 <matheussf300@gmail.com>
2026-03-13 13:58:17 -0300

    Exercicio 3 lógica inicial

---
+++ aula04/exercicio3aula04.py
@@ -0,0 +1,14 @@
+#Exercicio 3
+
+# 3. Rendimento do Taxista:
+# 3.1 Um motorista de táxi deseja calcular o rendimento de seu carro na praça.
+# 3.2 Sabendo-se que o preço do combustível é de R$ 6,15
+# 3.3 Escreva um programa para ler: a marcação do odômetro (km) no início do dia
+# 3.4 A marcação (km) no final do dia
+# 3.5 O número de litros de combustível gasto
+# 3.6 O valor total (R$) recebido dos passageiros. 
+# 3.7 Calcular e escrever: a média do consumo em km/L e o lucro (líquido) do dia.
+
+#Variaveis => combustivel, odometro (inicio, fim), combustivel_gasto, valor_total, media_consumo e lucro_liq
+
+combustivel= 6,15
