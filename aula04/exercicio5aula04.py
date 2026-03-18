8c8d2495093b46bfe309d2785069f7e6dd174f35
matheussf100 <matheussf300@gmail.com>
2026-03-18 07:58:17 -0300

    Exercicio 5 da aula 04

--- aula04/exercicio5aula04.py
+++ aula04/exercicio5aula04.py
@@ -1,1 +1,23 @@
+# variaveis => entrada(nota1, nota2, optativa) , calculo (media) , saida (resultado)
 
+nota1= float(input("Digite a nota da primeira prova"))
+nota2= float(input("Digite a nota da segunda prova"))
+opt= abs(float(input("Digite a nota da optativa:")))
+
+if opt != -1:
+    if nota1 < nota2:
+        nota1 = opt  
+    else:
+        nota2 = opt  
+
+media = (nota1 + nota2) / 2
+
+if media >= 6.0:
+    resultado = "Aprovado"
+elif media < 3.0:
+    resultado = "Reprovado"
+else:
+    resultado = "Recuperação"
+
+print(f"\nMédia final: {media:.1f}")
+print(f"Situação: {resultado}")
