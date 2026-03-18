7b22005873cdb4349dd32e0e09a251127d904a05
matheussf100 <matheussf300@gmail.com>
2026-03-18 07:56:32 -0300

    Exercicio 4 da aula 04

---
+++ aula04/exercicio4aula04.py
@@ -0,0 +1,25 @@
+codigo=5
+
+match codigo:
+    case 1:
+        print("Sul")
+    case 2:
+        print("Norte")
+    case 3:
+        print("Leste")
+    case 4:
+        print("Oeste")
+    case 5:
+        print("Nordeste")
+    case 6:
+        print("Nordeste")
+    case 7:
+        print("Sudeste")
+    case 8:
+        print("Sudeste")
+    case 9:
+        print("Sudeste")
+    case 10:
+        print("Centro-Oeste")
+    case 11:
+        print("Noroeste")
